42af56f4a9a5c0e667085c9c583d84c6ecbef01f
unknown <ceasergamer9@gmail.com>
2021-07-07 17:13:29 +0545

    admin layout

--- src/Component/Layout/Body/Body.js
+++ src/Component/Layout/Body/Body.js
@@ -1,26 +1,49 @@
 import Signup from './Signup';
 import Login from './Login';
-import Home from '../Common/home';
-import {BrowserRouter as Router,Switch,Route } from 'react-router-dom'
-const {Component}=require("react");
-
-class Body extends Component {
-  
-
-    render() {
-        return(
-          <div>
-              {/* <Route path="/" exact component={Home}/> */}
-              <Route path='/signup' component={Signup} />
-              <Route path='/login' component={Login} />
-              
-
-          </div>
+// import Home from '../Common/home';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom'
+import AdminLogin from '../Adminpannel/Adminlogin';
+import DashboardCharts from '../Adminpannel/Admindashboard';
 
 
-        )
+
+const Routes = ({ location }) => {
+
+    const checkRoute = () => {
+
+        const splitLocation = location.pathname.split("/")
+        if (splitLocation.length > 1) {
+            if (splitLocation[1] === "admin") {
+                return true;
+            }
+        }
+
+        return false;
     }
 
-}
+            return (
+                <>
+                <Switch>
+                    {/* <Route path="/" exact component={Home}/> */}
+                    <Route path='/signup' component={Signup} />
+                    <Route path='/login' component={Login} />
 
-export default Body;
+                    {/* For Admin */}
+                    <Route path="/admin/" render={({ location, history }) => (
+                        <main id="wrapper" className="wrapper">
+
+                            <Route path="/admin/" exact component={AdminLogin} />
+                            <Route path="/admin/dashboard" component={DashboardCharts} />
+
+
+                        </main>
+                    )} />
+                </Switch>
+                {/* {!checkRoute && "/admin'" && <Footer/>} */}
+                </>
+
+
+            )
+        }
+
+export default withRouter(Routes);
