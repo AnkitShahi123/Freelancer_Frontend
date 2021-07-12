983065a8ae69c102c9e49c2ebbae264f37d640c2
aavashneupane <aavashneupane4@gmail.com>
2021-07-12 12:28:43 +0545

    profile api fixed

--- src/Component/Layout/Common/profile.js
+++ src/Component/Layout/Common/profile.js
@@ -14,7 +14,7 @@ export default class profile extends Component {
   async componentDidMount() {
     await axios({
       method: "get",
-      url: "https://freelancerbackend.herokuapp.com",
+      url: "https://freelancerbackend.herokuapp.com/clientProfile",
       headers: { authorization: `Bearer ${localStorage.getItem("token")}` },
     })
       .then((response) => {
@@ -31,7 +31,7 @@ export default class profile extends Component {
   async componentDidMount() {
     await axios({
       method: "get",
-      url: "https://freelancerbackend.herokuapp.com",
+      url: "https://freelancerbackend.herokuapp.com/freelancerProfile",
       headers: { authorization: `Bearer ${localStorage.getItem("token")}` },
     })
       .then((response) => {
